68f3b8caeb37dea655d2bbb488500a5135c4dd12
Gtoasted <arne@vonlagun.de>
2024-05-14 10:37:04 +0200

    Removed redundant module

--- machines/alpha-centauri/configuration.nix
+++ machines/alpha-centauri/configuration.nix
@@ -11,9 +11,6 @@
 
   networking.hostName = "alpha-centauri"; # Define your hostname.
 
-  # Install firefox.
-  programs.firefox.enable = true;
-
   # Home Manager config
   home-manager = {
     extraSpecialArgs = { inherit inputs; };
