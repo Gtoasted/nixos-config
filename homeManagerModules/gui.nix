05feda9f4ba691a845df9c04f04dc9e72a962b73
Gtoasted <arne@vonlagun.de>
2024-05-16 19:58:58 +0200

    Refactored, configured waybar

--- homeManagerModules/gui.nix
+++ homeManagerModules/gui.nix
@@ -9,12 +9,4 @@
     enable = true;
     defaultEditor = true;
   };
-
-  wayland.windowManager.hyprland = {
-    enable = true;
-    package = pkgs.hyprland;
-    xwayland.enable = true;
-    systemd.enable = true;
-    extraConfig = lib.fileContents ./hypr/hyprland.conf;
-  };
 }
---
+++ homeManagerModules/hypr.nix
@@ -0,0 +1,18 @@
+# homeManagerModules/hypr.nix: Hyprland and supplementary modules
+
+{ pkgs, lib, ... }: {
+  wayland.windowManager.hyprland = {
+    enable = true;
+    package = pkgs.hyprland;
+    xwayland.enable = true;
+    systemd.enable = true;
+    extraConfig = lib.fileContents ../dotfiles/hypr/hyprland.conf;
+  };
+
+  programs.waybar = {
+    enable = true;
+    package = pkgs.waybar;
+    settings = lib.fileContents ../dotfiles/waybar/config;
+  }
+}
+
