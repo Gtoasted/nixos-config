d079cb385ee327a5c947dcfa4eb3bc15ffd64a70
Gtoasted <arne@vonlaguna.de>
2024-10-21 21:41:30 +0200

    Nixified hyprland

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -16,12 +16,168 @@
     wayland.windowManager.hyprland = {
       enable = true;
       package = pkgs.hyprland;
-      # plugins = [
-      #   inputs.hyprgrass.packages.${pkgs.system}.default
-      # ];
       xwayland.enable = true;
       systemd.enable = true;
-      extraConfig = lib.fileContents ../config/hypr/hyprland.conf;
+      settings = {
+        exec-once = "waybar & hyprlock & hypridle";
+        "$terminal" = "kitty";
+        "$fileManager" = "dolphin";
+        "$menu" = "wofi --show drun";
+
+        env = [
+          "XCURSOR_SIZE,24"
+          "QT_QPA_PLATFORMTHEME,qt5ct"
+        ];
+
+        monitor = [
+          "eDP-1,1920x1080@60,0x0,1"
+          ",preferred,auto,1"
+        ];
+
+        input = {
+            kb_layout = "de";
+            kb_variant = "nodeadkeys";
+            follow_mouse = 1;
+            touchpad.natural_scroll = "yes";
+            sensitivity = 0;
+        };
+
+        general = {
+          gaps_in = 2;
+          gaps_out = 5;
+          border_size = 1;
+          "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+          "col.inactive_border" = "rgba(595959aa)";
+          layout = "dwindle";
+          allow_tearing = "false";
+        };
+
+        decoration = {
+            rounding = 2;
+            
+            blur = {
+                enabled = true;
+                size = 3;
+                passes = 1;
+            };
+
+            drop_shadow = "yes";
+            shadow_range = 4;
+            shadow_render_power = 3;
+            "col.shadow" = "rgba(1a1a1aee)";
+        };
+
+        animations = {
+          enabled = "yes";
+          bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+          animation = [
+            "windows, 1, 7, myBezier"
+            "windowsOut, 1, 7, default, popin 80%"
+            "border, 1, 10, default"
+            "borderangle, 1, 8, default"
+            "fade, 1, 7, default"
+            "workspaces, 1, 6, default"
+          ];
+        };
+
+        dwindle = {
+            pseudotile = "yes";
+            preserve_split = "yes";
+        };
+
+        master = {
+            new_is_master = true;
+        };
+
+        gestures = {
+            workspace_swipe = true;
+            workspace_swipe_fingers = 4;
+        };
+        misc.force_default_wallpaper = -1;
+        device = {
+            name = "epic-mouse-v1";
+            sensitivity = -0.5;
+        };
+        windowrulev2 = [
+          "suppressevent maximize, class:.*"
+          "float, class:(nwg-displays)"
+        ];
+
+        "$mainMod" = "SUPER";
+        bind = [
+          "$mainMod, C, killactive, "
+          "$mainMod, V, togglefloating, "
+          "$mainMod, P, pseudo,"
+          "$mainMod, J, togglesplit,"
+
+          "$mainMod, Q, exec, $terminal"
+          "$mainMod, E, exec, $fileManager"
+          "$mainMod, R, exec, $menu"
+          "$mainMod, F, exec, firefox"
+
+          "$mainMod, left, movefocus, l"
+          "$mainMod, right, movefocus, r"
+          "$mainMod, up, movefocus, u"
+          "$mainMod, down, movefocus, d"
+
+          "SUPER SHIFT, left, movewindow, l"
+          "SUPER SHIFT, right, movewindow, r"
+          "SUPER SHIFT, up, movewindow, u"
+          "SUPER SHIFT, down, movewindow, d "
+
+          "$mainMod, 1, workspace, 1"
+          "$mainMod, 2, workspace, 2"
+          "$mainMod, 3, workspace, 3"
+          "$mainMod, 4, workspace, 4"
+          "$mainMod, 5, workspace, 5"
+          "$mainMod, 6, workspace, 6"
+          "$mainMod, 7, workspace, 7"
+          "$mainMod, 8, workspace, 8"
+          "$mainMod, 9, workspace, 9"
+          "$mainMod, 0, workspace, 10"
+
+          "$mainMod SHIFT, 1, movetoworkspace, 1"
+          "$mainMod SHIFT, 2, movetoworkspace, 2"
+          "$mainMod SHIFT, 3, movetoworkspace, 3"
+          "$mainMod SHIFT, 4, movetoworkspace, 4"
+          "$mainMod SHIFT, 5, movetoworkspace, 5"
+          "$mainMod SHIFT, 6, movetoworkspace, 6"
+          "$mainMod SHIFT, 7, movetoworkspace, 7"
+          "$mainMod SHIFT, 8, movetoworkspace, 8"
+          "$mainMod SHIFT, 9, movetoworkspace, 9"
+          "$mainMod SHIFT, 0, movetoworkspace, 10"
+
+          "$mainMod, S, togglespecialworkspace, magic"
+          "$mainMod SHIFT, S, movetoworkspace, special:magic"
+
+          "$mainMod, mouse_down, workspace, e+1"
+          "$mainMod, mouse_up, workspace, e-1"
+
+          "$mainMod, t, togglegroup"
+          "$mainMod, tab, changegroupactive, f"
+          "$mainMod SHIFT, tab, changegroupactive, b"
+
+          ", XF86AudioMute, exec, amixer sset 'Master' togglemute # F1"
+          ", XF86AudioLowerVolume, exec, amixer sset 'Master' 5%- # F2"
+          ", XF86AudioRaiseVolume, exec, amixer sset 'Master' 5%+ # F3"
+          ", XF86MonBrightnessDown, exec, brightnessctl set 5%- # F4"
+          ", XF86MonBrightnessUp, exec, brightnessctl set +5% # F5"
+
+          ", Print, exec, hyprshot -m region"
+
+          ", XF86PowerOff, exec, systemctl suspend"
+          "$mainMod, L, exec, loginctl lock-session"
+          "$mainMod, M, exec, pkill .nwg-displays-w || nwg-displays"
+          "SUPER, V, exec, cliphist list | wofi --dmenu | cliphist decode | wl-copy"
+        ];
+
+        bindm = [
+          "$mainMod, mouse:272, movewindow"
+          "$mainMod, surface:3, movewindow"
+          "$mainMod, mouse:273, resizewindow"
+        ];
+      };
+      # extraConfig = lib.fileContents ../config/hypr/hyprland.conf;
     };
 
     services.mako = {
