aba025c0fb5aa40e3a4d0c22099cc87cbd69111b
Gtoasted <arne@vonlaguna.de>
2024-11-13 11:16:08 +0100

    Removed obsolete flake contents

--- flake.nix
+++ flake.nix
@@ -18,17 +18,6 @@
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-
-    # hyprland = {
-    #   url = "github:hyprwm/Hyprland";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
-
-    # hyprgrass = {
-       # url = "github:horriblename/hyprgrass";
-       # inputs.hyprland.follows = "hyprland"; # IMPORTANT
-    # };
   };
 
   outputs = { self, nixpkgs, sops-nix, ... }@inputs: {
