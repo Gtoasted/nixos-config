3c739ac491dc06dcd6432086dc636a6b2c5501dd
Gtoasted <arne@vonlaguna.de>
2024-11-13 15:40:50 +0100

    Finally made easyroam work

--- modules/system/wifi.nix
+++ modules/system/wifi.nix
@@ -1,7 +1,24 @@
 { config, sops, pkgs, ... }: {
-  sops.secrets.wifi = {
-    format = "binary";
-    sopsFile = ../../secrets/wifi;
+  sops.secrets = {
+    wifi = {
+      format = "binary";
+      sopsFile = ../../secrets/wifi;
+    };
+
+    easyroam_client_cert = {
+      format = "binary";
+      sopsFile = ../../secrets/easyroam_client_cert.pem;
+    };
+
+    easyroam_client_key = {
+      format = "binary";
+      sopsFile = ../../secrets/easyroam_client_key.pem;
+    };
+
+    easyroam_root_ca = {
+      format = "binary";
+      sopsFile = ../../secrets/easyroam_root_ca.pem;
+    };
   };
 
   environment.systemPackages = [
@@ -20,6 +37,19 @@
       "The promised LAN".pskRaw = "ext:thepromisedlan";
       "Vodafone-5AE1".pskRaw = "ext:vodafone5ae1";
       "Gastzugang Marienhof".pskRaw = "ext:gastzugangmarienhof";
+      eduroam.auth = ''
+        key_mgmt=WPA-EAP
+        proto=WPA2
+        eap=TLS
+        pairwise=CCMP
+        group=CCMP
+        identity="1122784270474415738@easyroam-pca.uni-duesseldorf.de"
+        altsubject_match="DNS:easyroam.eduroam.de"
+        ca_cert="/run/secrets/easyroam_root_ca"
+        client_cert="/run/secrets/easyroam_client_cert"
+        private_key="/run/secrets/easyroam_client_key"
+        private_key_passwd=""
+      '';
     };
   };
 }
