ac94ca96113c8a46cb3efc621dd43ae05e17508e
Gtoasted <arne@vonlaguna.de>
2025-01-09 23:15:23 +0100

    add hyprland nixos module

---
+++ nixosModules/hyprland.nix
@@ -0,0 +1,14 @@
+{ config, lib, ... }: {
+	options.gtoasted.hyprland = {
+		enable = lib.mkEnableOption "Enable Hyprland.";
+	};
+
+	config = lib.mkIf config.gtoasted.hyprland.enable {
+		programs.hyprland = {
+			enable = true;
+			withUWSM = true;
+		};
+	};
+}
+
+
