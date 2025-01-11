69eaa272c89746f825875041399e667e35aac5bc
Gtoasted <arne@vonlaguna.de>
2025-01-11 22:06:21 +0100

    nixvim: enable ltex

--- homeModules/nixvim/plugins/lsp.nix
+++ homeModules/nixvim/plugins/lsp.nix
@@ -9,17 +9,14 @@
 			servers = {
 				lua_ls.enable = true;
 				nixd.enable = true;
-
-				# I found a total of three LaTeX Servers
-				# digestif.enable = true;
-				# ltex = {
-				#   enable = true;
-				#   settings = {
-				#     enabled = [ "latex" "tex" "text"];
-				#     completionEnabled = true;
-				#     language = "en-US de-DE";
-				#   };
-				# };
+				ltex = {
+				  enable = true;
+				  settings = {
+				    enabled = [ "latex" "tex" "text"];
+				    completionEnabled = true;
+				    language = "de-DE";
+				  };
+				};
 				texlab.enable = true;
 			};
 		};
