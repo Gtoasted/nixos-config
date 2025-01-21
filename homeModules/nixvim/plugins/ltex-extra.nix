8ecf721acb679d1970fe106ca23bc7d594d5805e
Gtoasted <arne@vonlaguna.de>
2025-01-21 09:59:34 +0100

    add ltex-extra

---
+++ homeModules/nixvim/plugins/ltex-extra.nix
@@ -0,0 +1,17 @@
+{ config, lib, ...}: {
+	options.gtoasted.neovim.plugins.ltex-extra = {
+		enable = lib.mkEnableOption "Enable the ltex neovim plugin.";
+	};
+
+	config = lib.mkIf config.gtoasted.neovim.plugins.ltex-extra.enable {
+		plugins.ltex-extra = {
+			enable = true;
+			settings = {
+				init_check = true;
+				load_langs = [
+					"de-DE"
+				];
+			};
+		};
+	};
+}
