5de3662b933760726ad5e041fea217bc01a5168a
Gtoasted <arne@vonlaguna.de>
2025-02-15 18:49:21 +0100

    nixvim: change floaterm keybinds

--- homeModules/nixvim/keymaps.nix
+++ homeModules/nixvim/keymaps.nix
@@ -16,16 +16,17 @@
     {key = "<S-Tab>"; action = "<cmd>bp<CR>"; mode = "n";}
 		# Other
 		{key = "<Esc>"; action = "<cmd>noh<CR>"; mode = "n";} # Clear highlights
-    {key = "<C-Space>"; action = "<C-\\><C-n>"; mode= "t";} # Exit terminal mode
+    # {key = "<C-Space>"; action = "<C-\\><C-n>"; mode= "t";} # Exit terminal mode
     # Plugins
 		# comment
 		{key = "<Leader>/"; action = "<Plug>(comment_toggle_linewise_current)"; mode = "n";}
 		{key = "<Leader>/"; action = "<Plug>(comment_toggle_linewise_visual)"; mode = "v";}
     # floaterm
+    {key = "<C-Space>"; action = "<cmd>FloatermToggle<CR>"; mode="n";}
+    {key = "<C-Space>"; action = "<cmd>FloatermToggle<CR>"; mode="t";}
     {key = "<Leader><CR>"; action = "<cmd>FloatermNew<CR>"; mode="n";}
     {key = "<Leader>fn"; action = "<cmd>FloatermNext<CR>"; mode="n";}
     {key = "<Leader>fp"; action = "<cmd>FloatermPrev<CR>"; mode="n";}
-    {key = "<Leader>ft"; action = "<cmd>FloatermToggle<CR>"; mode="n";}
     {key = "<Leader>fk"; action = "<cmd>FloatermKill<CR>"; mode="n";}
     # nvim-tree
     {key = "<C-n>"; action = "<cmd>NvimTreeToggle<CR>"; mode = "n";}
