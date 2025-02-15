9773e8735eb4449d8d751a6b52d2da92aa9dc988
Gtoasted <arne@vonlaguna.de>
2025-02-16 00:51:54 +0100

    nixvim: remove old lua snippets

--- modules/home/nixvim/snippets/lua.lua
+++ modules/home/nixvim/snippets/lua.lua
@@ -13,7 +13,7 @@ local rep = require("luasnip.extras").rep
 return {
   s({trig="fsnip", snippetType="autosnippet", dscr="Snippet to create snippets"},
     fmta([[
-    s({trig="<>", snippetType="<>", dscr="<>", condition=math},
+    s({trig="<>", snippetType="<>", dscr="<>"},
       fmta(<>,
       {<>}
       )
@@ -25,30 +25,8 @@ return {
 
   s({trig="tsnip", snippetType="autosnippet", dscr="Snippet that inserts text"},
     fmta([[
-    s({trig="<>", snippetType="autosnippet", dscr="<>", condition=math},
+    s({trig="<>", snippetType="autosnippet", dscr="<>"},
       {t("<>")}
-    ),
-    ]],
-    {i(1), i(2), i(3)}
-    )
-  ),
-
-  s({trig="lsnip", snippetType="autosnippet", dscr="Snippet that inserts a special letter"},
-    fmta([[
-    s({trig="<>", snippetType="autosnippet", dscr="<>", condition=math},
-      {t("<>")}
-    ),
-    ]],
-    {i(1), i(2), i(3)}
-    )
-  ),
-
-  s({trig="msnip", snippetType="autosnippet", dscr="Snippet that modifies preceding text"},
-    fmta([[
-    s({trig="(.*)%.<>", regTrig=true, snippetType="autosnippet", dscr="<>", condition=math},
-      {f(function (_, snip)
-        return snip.captures[1] .. "<>"
-      end)}
     ),
     ]],
     {i(1), i(2), i(3)}
