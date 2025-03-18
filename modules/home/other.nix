3ea20208b65a95e1fe78bf056b3bb2239580f664
Gtoasted <arne@vonlaguna.de>
2025-03-18 13:34:57 +0100

    disable IdeaVIM

--- modules/home/other.nix
+++ modules/home/other.nix
@@ -8,7 +8,7 @@
 		home.packages = let
       plg = jetbrains-plugins.idea-ultimate."2024.3.3"; 
       intellij = ( pkgs.jetbrains.plugins.addPlugins pkgs.jetbrains.idea-ultimate [
-          plg.IdeaVIM
+          # plg.IdeaVIM
           plg."com.intellij.spring"
           plg."com.intellij.spring.boot"
           plg."com.intellij.spring.boot.initializr"
