884f9e1ce0dfc201545f45f02b96143cc44fa379
Gtoasted <arne@vonlaguna.de>
2025-03-22 13:56:28 +0100

    remove ssh keys

--- modules/home/ssh.nix
+++ modules/home/ssh.nix
@@ -4,30 +4,12 @@
 	};
 
 	config = lib.mkIf config.gtoasted.ssh.enable {
-		sops.secrets = {
-			ssh_secret = {
-				format = "binary";
-				sopsFile = ../../secrets/id_ed25519;
-				mode = "0700";
-				path = ".ssh/id_ed25519";
-			};
-
-			ssh_public = {
-				format = "binary";
-				sopsFile = ../../secrets/id_ed25519.pub;
-				mode = "0700";
-				path = ".ssh/id_ed25519.pub";
-			};
-		};
-
     services.ssh-agent.enable = true;
-
-    home.packages = [ pkgs.cloudflared ];
 
     programs.ssh = {
       enable = true;
       matchBlocks.arcalis = {
-        proxyCommand = "cloudflared access ssh --hostname ssh-arcalis.gtoasted.de";
+        proxyCommand = "${lib.getExe pkgs.cloudflared} access ssh --hostname ssh-arcalis.gtoasted.de";
         user = "arne";
       };
     };
