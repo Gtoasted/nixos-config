91bc7bfe922f1fc5f152dc57e612358a71bccf82
Gtoasted <arne@vonlaguna.de>
2025-03-22 14:44:52 +0100

    change qt style to adwaita

--- modules/home/rice.nix
+++ modules/home/rice.nix
@@ -19,7 +19,7 @@
       };
       targets.qt = lib.mkIf cfg.qt {
         enable = true;
-        platform = "gnome";
+        platform = "adwaita";
       };
       targets.librewolf.profileNames = [
         "default"
