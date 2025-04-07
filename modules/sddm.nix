4711b5b08316cd73a86ff006022c1d1253c6e45c
Gtoasted <arne@vonlaguna.de>
2025-04-07 20:25:13 +0200

    sddm-astronaut now has a setting for its theme

--- modules/sddm.nix
+++ modules/sddm.nix
@@ -4,27 +4,18 @@
 	};
 
 	config = lib.mkIf config.gtoasted.sddm.enable {
-    environment.systemPackages = let
-      sddm-astronaut = pkgs.sddm-astronaut.overrideAttrs (final: prev: {
-        installPhase =
-          let
-            theme = "cyberpunk";
-            basePath = "$out/share/sddm/themes/sddm-astronaut-theme";
-          in
-            prev.installPhase + ''
-              ${lib.getExe pkgs.gnused} -ie "/^ConfigFile/c ConfigFile=Themes/${theme}.conf" ${basePath}/metadata.desktop
-            '';
-      });
-    in with pkgs; [
+    environment.systemPackages = with pkgs; [
       kdePackages.qtmultimedia
-      sddm-astronaut
+      (sddm-astronaut.override {
+        embeddedTheme = "cyberpunk";
+      })
     ];
 
 		services.displayManager.sddm = {
 			enable = true;
+      package = pkgs.kdePackages.sddm;
 			wayland.enable = true;
       theme = "sddm-astronaut-theme";
 		};
 	};
 }
-
