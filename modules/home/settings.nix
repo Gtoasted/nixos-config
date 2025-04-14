f36b943b6c173deceab0832e5e21736ec7b8d0a6
Gtoasted <arne@vonlaguna.de>
2025-04-14 22:01:23 +0200

    add default browser to settings.nix
    TODO: make hyprland follow this and add other default programs

--- modules/home/settings.nix
+++ modules/home/settings.nix
@@ -1,4 +1,4 @@
-{ config, lib, ...}: {
+{ config, lib, pkgs, ...}: {
   options.gtoasted.settings = with lib; {
     autostart = mkOption {
       default = [ ];
@@ -8,11 +8,29 @@
         List of commands to be automatically executed on startup.
       '';
     };
+    browser = mkOption {
+      type = types.str;
+      default = lib.getExe pkgs.firefox;
+    };
   };
 
   config = let
     cfg = config.gtoasted.settings;
   in {
     gtoasted.hyprland.autostart = cfg.autostart;
+    xdg.mimeApps.defaultApplications = let
+      browser = pkgs.makeDesktopItem rec {
+        name = "browser";
+        desktopName = name;
+        exec = browser;
+        terminal = false;
+      };
+    in {
+      "text/html" = browser.name;
+      "x-scheme-handler/http" = browser.name;
+      "x-scheme-handler/https" = browser.name;
+      "x-scheme-handler/about" = browser.name;
+      "x-scheme-handler/unknown" = browser.name;
+    };
   };
 }
