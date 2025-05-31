26827244c77de466f399b417eb7fe2b61b0c9a41
Gtoasted <arne@vonlaguna.de>
2025-05-31 18:41:16 +0200

    some steam options

--- modules/steam.nix
+++ modules/steam.nix
@@ -1,10 +1,17 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 {
   options.gtoasted.steam = {
     enable = lib.mkEnableOption "Enable steam.";
   };
 
   config = lib.mkIf config.gtoasted.steam.enable {
-    programs.steam.enable = true;
+    programs.steam = {
+      enable = true;
+      protontricks.enable = true;
+      gamescopeSession.enable = true;
+      extraCompatPackages = with pkgs; [
+        proton-ge-bin
+      ];
+    };
   };
 }
