d96f42c2543651648dec850eb8e7e58e49ba8254
Gtoasted <arne@vonlaguna.de>
2025-06-07 14:10:01 +0200

    --no-edit

--- modules/home/terminal.nix
+++ modules/home/terminal.nix
@@ -14,14 +14,46 @@
       wl-clipboard
       zip
       unzip
+
+      uutils-coreutils-noprefix
+      ripgrep
+      fd
+      eza
+      btop
     ];
 
     programs = {
-      bash.enable = true;
       kitty.enable = true;
+
+      fish = {
+        enable = true;
+        shellAbbrs = {
+          ls = "eza";
+          cd = "z";
+          cat = "bat";
+        };
+      };
+      bash = {
+        enable = true;
+        initExtra = ''
+        if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+        then
+          shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+          exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+        fi
+        '';
+      };
+
       starship = {
         enable = true;
         enableBashIntegration = true;
+        enableFishIntegration = true;
+      };
+      bat.enable = true;
+      zoxide = {
+        enable = true;
+        enableBashIntegration = true;
+        enableFishIntegration = true;
       };
     };
   };
