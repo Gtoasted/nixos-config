6c80cbb4d449e31569959ec4e9e964ac226a7c90
Gtoasted <arne@vonlaguna.de>
2025-06-28 15:36:05 +0200

    add authorized ssh key

--- modules/users/arne.nix
+++ modules/users/arne.nix
@@ -15,6 +15,9 @@
         "lp"
         "libvirtd"
       ];
+      openssh.authorizedKeys.keys = [
+        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCyGYjjrEqK5Z6fh/lS54c1elbfSMOZ/NXjfuKc5Vo84JEOkvBf2MeCDtW+bo4bS2NBBgXJODDBTZ56IqJZlc+LOqHg9dtZdNCEA/R6EtzGyf/uChSkLrQt6Wte9Svv2HykQawzCxG4fFGyK0acajX9dypqXR5uk/OohDipyY6EBB692dCqru3CkINbWi2NE4zXXqJuwO/JEk5Ywo/41nDZv4fgXmf91lBdjZYF6iA1F9l0PuPqo02/zuKoFcgh2OQUAkVkE0qB1QXvOxyjgxhOSvf1wCuaC69FRblW7wftT+hoxAOWy3LOoeFAd8V7eCEf4cFWsPeylo9FpYW9t02tjKuXfahmBlBT+Q0QDhsxf7FEwFAK+9iWDMhp3v9sMOs/Wsh8lpvAA1qR54pMuT35QigQ9qTXQb1qRw476/hhopngoQ10rAQPfI+XNUk/aiakhx08LmlgqsK1nNiNB42UKL3TSWAOu9jRArWTH2pL56jmGw/wKOs1geXbtT7Psec= (encrypted)"
+      ];
     };
 
     home-manager = {
